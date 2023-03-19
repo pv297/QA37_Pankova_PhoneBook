a8820d40542300dfbee835bb99db6f2271b33825
pv297 <p.v.2977187@gmail.com>
2023-03-19 12:54:39 +0200

    LoginTests classwork

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -4,8 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.Duration;
+
 public class ApplicationManager {
     WebDriver wd;
+    HelperUser helperUser;
+
 
 
     public void init(){
@@ -13,10 +17,15 @@ public class ApplicationManager {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         wd = new ChromeDriver(options);
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         wd.navigate().to("https://telranedu.web.app/");
+        helperUser = new HelperUser(wd);
     }
 
-
+    public HelperUser getHelperUser() {
+        return helperUser;
+    }
 
     public void stop(){
         wd.quit();
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,8 +1,44 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
+
+    @BeforeMethod
+    public void preCondition(){
+
+        if(app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+        }
+    }
+
+    @Test
+    public void loginSuccess(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("p.v.2977187@gmail.com","P29348092l@");
+        app.getHelperUser().submitLogin();
+        // time -----> singOut
+
+        // Assert
+
+       // Assert.assertEquals();
+       // Assert.assertNotEquals();
+        //Assert.assertTrue();
+       // Assert.assertFalse();
+
+      Assert.assertTrue(app.getHelperUser().isLogged());
+    }
+    @Test
+    public void loginSuccessModel(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("p.v.2977187@gmail.com","P29348092l@");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+
+    }
 
 
 }
