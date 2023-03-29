9753278244d67c6c324f14775420bb53159652a5
pv297 <p.v.2977187@gmail.com>
2023-03-29 21:21:25 +0300

    Negative tests + models

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,5 +1,6 @@
 package manager;
 
+import models.User;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,64 +10,105 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class HelperUser extends HelperBase {
+public class HelperUser extends HelperBase{
     public HelperUser(WebDriver wd) {
         super(wd);
     }
 
-    public void openLoginRegistrationForm() {
-//        WebElement loginTab = wd.findElement(By.xpath("//a[text()='LOGIN']"));
-//        // css - "a[href='/login']"
+    public void openLoginRegistrationForm(){
+//        WebElement loginTab = wd.findElement(By.cssSelector("a[href='/login']"));
+//                // xpath //a[text()='LOGIN']
 //
 //        loginTab.click();
         click(By.cssSelector("a[href='/login']"));
+    }
+
+    public void fillLoginRegistrationForm(String email,String password){
+//        WebElement emailInput =wd.findElement(By.name("email"));
+//        emailInput.click();
+//        emailInput.clear();
+//        emailInput.sendKeys(email);
+        type(By.name("email"),email);
+
+//        WebElement passwordInput= wd.findElement(By.xpath("//input[last()]"));
+//        passwordInput.click();
+//        passwordInput.clear();
+//        passwordInput.sendKeys(password);
+        type(By.xpath("//input[last()]"),password);
 
     }
 
-    public void fillLoginRegistrationForm(String email, String password) {
-//     WebElement emailInput = wd.findElement(By.name("email"));
-//     emailInput.click();
-//     emailInput.clear();
-//     emailInput.sendKeys(email);
-        type(By.name("email"), email);
-
-
-//     WebElement passwordInput = wd.findElement(By.xpath("//input[last()]"));
-//     passwordInput.click();
-//     passwordInput.clear();
-//     passwordInput.sendKeys(password);
-        type(By.xpath("//input[last()]"), password);
-
+    public void fillLoginRegistrationForm(User user){
+        type(By.name("email"), user.getEmail());
+        type(By.xpath("//input[last()]"), user.getPassword());
     }
 
-    public void submitLogin() {
+
+    public void submitLogin(){
         click(By.xpath("//button[text()='Login']"));
     }
 
     public boolean isLogged() {
         return isElementPresent(By.xpath("//button[text()='Sign Out']"));
-
+        // return false;
     }
 
-    public void logout() {
+    public void logout(){
         click(By.xpath("//button[text()='Sign Out']"));
     }
 
-    public boolean isAlertPresent(String massage) {
-        WebDriverWait wait = new WebDriverWait(wd, Duration.ofSeconds(5));
+    public boolean isAlertPresent(String message) {
+        WebDriverWait wait = new WebDriverWait(wd, Duration.ofSeconds(10));
 
         Alert alert = wait.until(ExpectedConditions.alertIsPresent());
-        if (alert != null && alert.getText().equals(massage)) {
-            System.out.println(alert.getText());
-            //click ok
-            //pause
-            alert.accept();
+        System.out.println(alert.getText());
+        if(alert != null && alert.getText().equals(message)){
 
-            //click cancel ---> alert.dismiss();
-            //type into alert ---> alert.sendKeys("hello");
+            // click ok
+            // pause
+            alert.accept();
+            // click cancel  --->  alert.dismiss();
+            // type into alert      -->alert.sendKeys("hello");
+
 
             return true;
         }
         return false;
+    }
+    public boolean isAlertPresent2(String message) {
+        WebDriverWait wait = new WebDriverWait(wd, Duration.ofSeconds(5));
+
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+       // System.out.println(alert.getText());
+        if(alert != null && alert.getText().contains(message)){
+
+            // click ok
+            // pause
+            alert.accept();
+            // click cancel  --->  alert.dismiss();
+            // type into alert      -->alert.sendKeys("hello");
+
+
+            return true;
+        }
+        return false;
+    }
+    public void submitRegistration() {
+        click(By.xpath("//button[text()='Registration']"));
+    }
+
+    public boolean isNoContactsHereDisplayed() {
+        WebDriverWait wait = new WebDriverWait(wd,Duration.ofSeconds(5));
+        boolean res =wait.until(ExpectedConditions.textToBePresentInElement(wd.findElement(By.cssSelector(".contact-page_message__2qafk>h1")),"No Contacts here!"));
+        return res;
+    }
+
+
+    public String getMessage(){
+
+        WebDriverWait wait = new WebDriverWait(wd, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOf(wd.findElement(By.cssSelector(".contact-page_message__2qafk>h1"))));
+
+        return wd.findElement(By.cssSelector(".contact-page_message__2qafk>h1")).getText();
     }
 }
